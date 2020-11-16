c0c9b902619e8035203a3cf5f6d20a1c1e225753
faiyazkhanwif <faiyazkhanwif@gmail.com>
2020-11-16 22:56:09 +0800

    Medium

--- src/hackerrankjava/VowelSubstringALT.java
+++ src/hackerrankjava/VowelSubstringALT.java
@@ -10,5 +10,27 @@ package hackerrankjava;
  * @author faiya
  */
 public class VowelSubstringALT {
-    
+
+    public static void main(String[] args) {
+        String maxstr = "";
+        int maxcnt = -1;
+        String m = "caberqiitefg";
+        for (int i = 0; i <= m.length() - 5; i++) {
+            String temp = m.substring(i, i + 5);
+            //System.out.println("Ekhon: ----" + temp);
+            int tempcnt = 0;
+            for (int j = 0; j < temp.length(); j++) {
+                char c = temp.charAt(j);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    tempcnt += 1;
+                }
+            }
+            //System.out.println("point: "+tempcnt);
+            if (tempcnt > maxcnt) {
+                maxstr = temp;
+                maxcnt = tempcnt;
+            }
+        }
+        System.out.println(maxstr);
+    }
 }
