dd34c701c909f2df6cdd53ddbf66dac6062b320a
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-02-05 04:54:39 +0800

    Easy

---
+++ src/firecodeio/PalindromeTester.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firecodeio;
+
+/**
+ *
+ * @author faiya
+ */
+public class PalindromeTester {
+
+    public static boolean isStringPalindrome(String str) {
+        if (str == null || str.isEmpty()) {
+            return true;
+        }
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += String.valueOf(str.charAt(i));
+        }
+        if (rev.equalsIgnoreCase(str)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isStringPalindrome("madam"));
+    }
+}
