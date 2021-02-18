98f2e0c8d1378161d0c0ea3ebeda81956122203f
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-02-19 00:32:11 +0800

    Medium

---
+++ src/firecodeio/level3/JamIntoBST.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firecodeio.level3;
+
+/**
+ *
+ * @author faiya
+ */
+//Oracle
+public class JamIntoBST {
+
+    class TreeNode {
+
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+
+        TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode insert(TreeNode root, int data) {
+        if (root == null) {
+            TreeNode nd = new TreeNode(data);
+            return nd;
+        } else {
+            if (data < root.data) {
+                root.left = insert(root.left, data);
+            } else if (data > root.data) {
+                root.right = insert(root.right, data);
+            }
+        }
+        return root;
+    }
+}
