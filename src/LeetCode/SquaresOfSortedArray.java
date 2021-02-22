cac97e39cc96f4e5c3c37706bcbbcea9b8d314e8
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-02-22 08:07:37 +0800

    Easy

---
+++ src/LeetCode/SquaresOfSortedArray.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode;
+
+/**
+ *
+ * @author faiya
+ */
+public class SquaresOfSortedArray {
+
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
