94381271185546c6c8d08f20ece2952c32325ef6
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-02-24 15:44:52 +0800

    Easy

--- src/LeetCode/ReverseALinkedList.java
+++ src/LeetCode/ReverseALinkedList.java
@@ -10,5 +10,57 @@ package LeetCode;
  * @author faiya
  */
 public class ReverseALinkedList {
-    
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    //Iterative
+    /*
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+     */
+    //Recursive
+    public ListNode reverseList(ListNode head) {
+        return work(head, null);
+    }
+
+    public ListNode work(ListNode head, ListNode newHead) {
+        if (head == null) {
+            return newHead;
+        }
+        ListNode curr = head;
+        ListNode next = curr.next;
+        curr.next = newHead;
+        newHead = curr;
+        curr = next;
+        return work(curr, newHead);
+    }
 }
