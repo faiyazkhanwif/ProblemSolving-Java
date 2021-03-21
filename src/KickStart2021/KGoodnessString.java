bd6f1780ce2e0a68aead11bcdd61ab1d01acc17f
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-03-21 14:11:18 +0800

    Medium

---
+++ src/KickStart2021/KGoodnessString.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KickStart2021;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author faiya
+ */
+public class KGoodnessString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            String str = in.next();
+            //System.out.println("---------");
+            //System.out.println(str);
+            int gds = 0;
+            for (int j = 1; j <= n; j++) {
+                if (j <= n / 2) {
+                    //System.out.println("j = " + j);
+                    int temp = n - j + 1;
+                    //System.out.println("temp = " + temp);
+                    temp = temp - 1;
+                    if (temp <= n && str.charAt(j - 1) != str.charAt(temp)) {
+                        //System.out.println(str.charAt(j - 1) + "---------" + str.charAt(temp));
+                        gds++;
+                    }
+                }
+            }
+            //System.out.println(gds);
+            if (gds >= k) {
+                System.out.println("Case #" + (i + 1) + ": " + 0);
+            } else {
+                double ans = k - gds;
+                double ans2 = (ans/2);
+                System.out.println("Case #" + (i + 1) + ": " + (int)Math.ceil(ans2));
+            }
+
+        }
+    }
+}
