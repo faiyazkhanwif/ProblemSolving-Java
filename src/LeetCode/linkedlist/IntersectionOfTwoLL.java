0caab0e4082d2f66043bcb2055c9b5310518c3a3
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-04-24 23:02:22 +0800

    Easy

---
+++ src/LeetCode/linkedlist/IntersectionOfTwoLL.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.linkedlist;
+
+/**
+ *
+ * @author faiya
+ */
+public class IntersectionOfTwoLL {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode currA = headA;
+        while (currA != null) {
+            ListNode currB = headB;
+            while (currB != null) {
+                if (currA == currB) {
+                    return currA;
+                }
+                currB = currB.next;
+            }
+            currA = currA.next;
+        }
+        return null;
+    }
+
+    /* Efficient Solution
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = length(headA), lenB = length(headB);
+        // move headA and headB to the same start point
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        // find the intersection until end
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+     */
+}
