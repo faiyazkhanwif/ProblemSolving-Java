b241c10447c4236d261ef814309dbf04bdd523cb
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-08-17 13:52:15 +0800

    Medium

---
+++ src/roadtohundred/LC371.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadtohundred;
+
+/**
+ *
+ * @author faiya
+ */
+public class LC371 {
+
+
+    public static int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+}
