55f7a8dd4690e78941e2e742054324c0347be6bf
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-11-06 01:42:50 +0800

    8 kyu

---
+++ src/codewars/fundamentals/RemoveFirstandLastCharacter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codewars.fundamentals;
+
+/**
+ *
+ * @author faiya
+ */
+public class RemoveFirstandLastCharacter {
+
+    public static String remove(String str) {
+
+        return str.substring(1,str.length()-1);
+    }
+}
