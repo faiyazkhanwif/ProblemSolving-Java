132107c2d54e75ed27c3301cfadaf3b946af9edc
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-03-06 06:40:09 +0800

    Easy

---
+++ src/random/CodilityDemo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package random;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author faiya
+ */
+public class CodilityDemo {
+
+    public static int solution(int[] A) {
+        int N = A.length;
+        Set<Integer> set = new HashSet<>();
+        for (int a : A) {
+            if (a > 0) {
+                set.add(a);
+            }
+        }
+        for (int i = 1; i <= N + 1; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {5, 2, 7, 1, 2, 4};
+        solution(A);
+    }
+}
